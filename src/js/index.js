41c8e290673f4b61d9bc08f4a6e9da045b7c0960
Satvik Virmani <virmanisatvik01@gmail.com>
2020-10-13 19:54:57 +0530

    Added Slideshow

--- src/js/index.js
+++ src/js/index.js
@@ -2,5 +2,9 @@ $(document).ready(function() {
     $("#toggle-btn").click(function() {
         $(".mobile-nav").toggleClass("mobile-nav-active");
         $(".mobile-nav-li").toggleClass("mobile-nav-li-active");
+        $(".all-page-content").toggleClass("all-page-content-margin-subtract");
     });
 });
+$('.carousel').carousel({
+    interval: 5000,
+})
